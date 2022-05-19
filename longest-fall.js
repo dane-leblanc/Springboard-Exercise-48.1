1f1cf37d313acfeb724c321ff5f09a680d1b6895
Dane-LeBlanc <dane.leblanc@gmail.com>
2022-05-19 11:37:02 -0600

    completed further study

--- longest-fall.js
+++ longest-fall.js
@@ -1,3 +1,16 @@
 // add whatever parameters you deem necessary
-function longestFall() {
+function longestFall(nums) {
+  let longestFall = 0;
+  let currentFall = 1;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] < nums[i - 1]) {
+      currentFall++;
+    } else {
+      currentFall = 1;
+    }
+    longestFall = Math.max(longestFall, currentFall);
+  }
+  return longestFall;
 }
+
+console.log(longestFall([5]));
