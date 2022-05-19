8bba856d994fb4b2ef7564a0a101679221666cc8
Dane-LeBlanc <dane.leblanc@gmail.com>
2022-05-18 21:36:35 -0600

    completed tasks one and two

--- average-pair.js
+++ average-pair.js
@@ -1,3 +1,17 @@
 // add whatever parameters you deem necessary
-function averagePair() {
+function averagePair(nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+  while (left <= right) {
+    if ((nums[left] + nums[right]) / 2 === target) {
+      return true;
+    } else if ((nums[left] + nums[right]) / 2 < target) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+  return false;
 }
+
+console.log(averagePair([1, 2, 3], 2.5));
--- construct-note.js
+++ construct-note.js
@@ -1,3 +1,23 @@
 // add whatever parameters you deem necessary
-function constructNote() {
+// O (2M + N) where M is the length of str1 and N is the length of string 2
+
+function constructNote(str1, str2) {
+  const charFreq1 = frequencyCounter(str1);
+  const charFreq2 = frequencyCounter(str2);
+  for (let key of charFreq1.keys()) {
+    if (!charFreq2.get(key) || charFreq2.get(key) < charFreq1.get(key))
+      return false;
+  }
+  return true;
 }
+
+function frequencyCounter(str) {
+  let charFreq = new Map();
+  for (let char of str) {
+    let charCount = charFreq.get(char) || 0;
+    charFreq.set(char, charCount + 1);
+  }
+  return charFreq;
+}
+
+console.log(constructNote("aa", "dcba"));
